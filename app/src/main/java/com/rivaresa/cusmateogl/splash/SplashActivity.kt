86b54a268e665a457b1a7c40dc8fb4b5ec59c762
Leslin <it@finwintechnologies.com>
2021-07-01 17:47:35 +0530

    commit 1-07-21

--- app/src/main/java/com/rivaresa/cusmateogl/splash/SplashActivity.kt
+++ app/src/main/java/com/rivaresa/cusmateogl/splash/SplashActivity.kt
@@ -1,10 +1,11 @@
 package com.rivaresa.cusmateogl.splash
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
-import com.rivaresa.cusmateogl.BaseActivity
 import com.rivaresa.cusmateogl.R
 import com.rivaresa.cusmateogl.login.LoginActivity
 
@@ -12,12 +13,16 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.statusBarColor = resources.getColor(R.color.white);
+        }
 
         Handler().postDelayed({
             run {
                 val i = Intent(this@SplashActivity, LoginActivity::class.java)
                 startActivity(i)
+                overridePendingTransition(R.anim.fadein, R.anim.fadeout)
                 finish()
             }
         }, 3000)
